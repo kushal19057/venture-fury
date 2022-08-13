438e15a9af4ce9d41832d096b7caffa94d299e98
raghav-dwivedi <rd9008000@gmail.com>
2022-08-14 01:23:48 +0530

    added cors and body parser

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const CORS = require("cors");
+const BODY_PARSER = require("body-parser");
+
+app.use(CORS());
+app.use(BODY_PARSER.urlencoded({ extended: true }));
+app.use(BODY_PARSER.json());
 
 app.use(require("./routes/products/addProduct"));
 app.use(require("./routes/users/addUser"));
